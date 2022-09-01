5a4f35e9ea4eb02f141763faee974bd2721938e0
lucho9595 <lucianocoronel48@gmail.com>
2022-09-01 16:50:46 -0300

    Deploy

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Card.module.css"
 
-export default function Card({ id, name, image, genres, rating, background_image }) {
+export default function Card({ id, name, image, genres, rating, background_image, platform }) {
 
     return (
         <div className={styles.contain}>
@@ -10,6 +10,7 @@ export default function Card({ id, name, image, genres, rating, background_image
             <h3 className={styles.h3}>Name: {name.toLowerCase()}</h3>
             <h3 className={styles.h3}>Genre: {typeof genres[0] === "string" ? genres.join("  ") : genres?.map(e => `${e.name} `)}</h3>
             <h3 className={styles.h3}>Rating: {rating}</h3>
+            <h3>Platform: {typeof platform[0] === "string" ? platform.join("  ") : platform?.map(e => `${e.name} `)}</h3>
             <Link to={`/videogame/${id}`}>
                 <button className={styles.details} >Details</button>
             </Link>
