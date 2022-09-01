9f4f9d1483d1c6c4e4be61603e25ad131bfd99c4
lucho9595 <lucianocoronel48@gmail.com>
2022-09-01 14:37:30 -0300

    Deploy

--- src/Components/Filters/FilterAlpha.jsx
+++ src/Components/Filters/FilterAlpha.jsx
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React from "react";
 import { useDispatch} from "react-redux";
 import { filterAlphabet } from "../../Redux/Actions.js";
 import styles from "./FilterAlpha.module.css";
